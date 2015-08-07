12a57c11c63e7674b8f125b8d1638deddadd7321
Jonathan Lam <jdlam420@gmail.com>
2015-08-07 10:12:08 -0400

    Deals cards, calculates bets, player chooses options: adding to your current bet, deal cards, hit, stay, play again, or leave the table

--- README.md
+++ README.md
@@ -1,31 +1,37 @@
 ## Blackjack
 - A blackjack game made for you!
 - You play 1v1 vs the house
-- Multiple players functionality may be added later
-
-![Blackjack](http://globe-views.com/dcim/dreams/blackjack/blackjack-01.jpg)
-
 
 ## Currrent Goals
 - ~~Creates a deck~~
 - ~~Shuffles said deck~~
 - ~~Creates player and dealer~~
 - ~~Deals cards~~
-- Buttons
-  - Before a game has started: Start Game/Deal Cards
-  - Before a round starts: Different betting amounts
-  - Mid-Round: Hit, Stay, and Reset
-    - **Bonus:** Split or Double Down options?
-  - Post-Game: Play Again
+- ~~Evaluates cards~~
+- ~~Win and Loss condition~~
 - Visual Representations
   - Cards (Images?)
-  - Chips
+  - Bank and Current Bet
+- ~~Buttons~~
+  - ~~Before a game has started: Start Game/Deal Cards~~
+  - ~~Before a round starts: Different betting amounts~~
+  - ~~Mid-Round: Hit and Stay~~
+    - **Bonus:** Split or Double Down options?
+  - ~~Post-Game: Play Again or Leave Table~~
 
 ## User Stories
-- I click on the deal cards button
+- User clicks on chip buttons to enter betting amounts
+  - User is presented with a start the game or deal cards button
+- User clicks on the deal cards button
   - Cards are dealt to each player
   - Dealer has one card face down
-- I press the
+  - User is presented with hit or stay buttons
+- User clicks the hit button
+  - User is dealt another card
+  - Presented with either hit or stay buttons again
+- User clicks the stay button
+  - The dealer hits until he's above 16 or busts
+  - Dealer and User values are compared after if dealer hasn't busted
 
 
 ## Technologies Used
--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,4 +1,4 @@
-function deck() { // Deck constructor function
+function Deck() { // Deck constructor function
 
   // Defining each component of a card
   // The number/face value and the suit value
@@ -24,7 +24,7 @@ function deck() { // Deck constructor function
 
 // Temporary function to list all of the cards within my deck
 // Makes life easier in the midst of developing
-deck.prototype.listCards = function listCards() {
+Deck.prototype.listCards = function listCards() {
   for (var i=0; i<this.cardsArray.length; i++) {
     console.log(this.cardsArray[i]);
   }
@@ -33,7 +33,7 @@ deck.prototype.listCards = function listCards() {
 
 
 // Shuffle function
-deck.prototype.shuffle = function shuffle() {
+Deck.prototype.shuffle = function shuffle() {
   var shuffleIdx = this.cardsArray.length;
   var swapIdx;
   var temp;
@@ -52,19 +52,220 @@ deck.prototype.shuffle = function shuffle() {
   console.log('The cards have been shuffled\nWe\'re ready to play');
 }
 
-deck.prototype.dealCards = function dealCards() {
+
+// Cards are dealt out to the player and the dealer
+Deck.prototype.dealCards = function dealCards() {
+
+  // 2 cards each
+  // player first, then dealer.
   for (var i=0; i<2; i++) {
-    theDealer.hand.push(this.cardsArray.shift());
-    thePlayer.hand.push(this.cardsArray.shift());
+    this.hit(thePlayer);
+    this.hit(theDealer);
   }
   console.log("Cards have been dealt");
+  thePlayer.evaluateCards();
+  theDealer.evaluateCards();
 }
 
-function player() {
+Deck.prototype.hit = function hit(player) {
+  player.hand.push(this.cardsArray.shift());
+}
+
+// -----------------------------------------------------------------------------
+
+// Player Constructor Function
+
+function Player( name ) {
+  this.name = name;
   this.hand = [];
   this.bank = 100;
+  this.cardValue;
+  this.blackjack = false;
+  this.currentBet = 0;
+  this.bust = false;
 }
 
-var blackjackDeck = new deck();
-var thePlayer = new player();
-var theDealer = new player();
+// Player places bet at the beginning of the game
+Player.prototype.placeBet = function placeBet(bet) {
+  var bet = parseInt(bet);
+  if (bet > this.bank) {
+    console.log("You don't have enough");
+  } else {
+    this.bank -= bet;
+    this.currentBet += bet;
+    console.log("Your current bet is: " + this.currentBet);
+  }
+}
+
+// Function to evaluate the player's hand
+
+Player.prototype.evaluateCards = function evaluateCards() {
+
+  // creating my local variable singleValue and setting cardValue back to 0
+  var singleValue = [];
+  this.cardValue = 0;
+
+  for (var i=0; i<this.hand.length; i++) {
+    // Each card is split into 2 strings, the face/number, and then the suit
+    // Then it's pushed into the array singleValue
+    singleValue.push(this.faceValue(this.hand[i].split(' ')[0]));
+    this.cardValue += singleValue[i]; // each value is added to this.cardValue
+  }
+
+  // if busted, then double checking for any Aces
+  this.checkAces(singleValue);
+
+  // checks the player's cardValue to see if they busted or not
+  this.checkBust();
+  this.checkBlackjack();
+}
+
+Player.prototype.faceValue = function faceValue(face) {// converts cards into values
+  if (face === "A") {
+    return 11;
+  } else if (face === "K" || face === "Q" || face === "J") {
+    return 10;
+  } else {
+    return parseInt(face);
+  }
+}
+
+Player.prototype.checkAces = function checkAces(singleValue) {
+  if (this.cardValue > 21) {
+    // If there are aces, then it re-evaluates the cardValue
+    // adjustedValues checks for Aces, and converts them to a value of 1
+    if (singleValue.indexOf(11) !== -1) {
+      singleValue[singleValue.indexOf(11)] = 1;
+    }
+
+    // resets the cardValue to 0, then adds the values of adjusted singleValue again
+    this.cardValue = 0;
+    for (var i=0; i<singleValue.length; i++) {
+      this.cardValue += singleValue[i];
+    }
+  }
+}
+
+Player.prototype.stay = function stay() {
+  // Only thePlayer will be using the stay function
+  // Therefore, the dealer's entire turn is within this function
+
+  theDealer.evaluateCards();
+  while (theDealer.cardValue < 17 && theDealer.cardValue !== 0) {
+    blackjackDeck.hit(theDealer);
+    theDealer.evaluateCards();
+    console.log('Dealer while loop finished')
+  }
+  compareWin();
+}
+
+// Evaluates the players' hand for busting
+Player.prototype.checkBust = function checkBust() {
+  if (this.cardValue > 21) {
+    console.log(this.name + " has busted!"); // change these to alerts
+    this.cardValue = 0;
+    this.bust = true;
+    alert(this.name + " has busted!");
+  } else {
+    console.log(this.name + " has not busted! Keep playing playa");
+  }
+}
+
+Player.prototype.checkBlackjack = function checkBlackjack() {
+  if (this.cardValue === 21 && this.hand.length === 2) {
+    this.blackjack = true;
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+// Functions outside of the constructors/objects
+
+function compareWin() {
+  // Checks win conditions between the player and the dealer
+  console.log(thePlayer.name + ": " + thePlayer.cardValue);
+  console.log(theDealer.name + ": " + theDealer.cardValue);
+
+
+
+  if ((thePlayer.blackjack === true && theDealer.blackjack === true) || (thePlayer.cardValue === theDealer.cardValue)) {
+    // Tie
+    // push. Player receives his bet back
+    thePlayer.bank += thePlayer.currentBet;
+    thePlayer.currentBet = 0;
+    console.log("You tied with the dealer!\nYou received your money back")
+  } else if ((thePlayer.blackjack === true) || (thePlayer.cardValue > theDealer.cardValue)) {
+    // Player wins
+    thePlayer.bank += 2*thePlayer.currentBet;
+    console.log("You won $" + 2*thePlayer.currentBet)
+    thePlayer.currentBet = 0;
+  } else {
+    // Dealer wins
+    thePlayer.currentBet = 0;
+    console.log("You lost...")
+  }
+  $('.inGame').hide();
+  $('.postGame').show();
+}
+
+function bindButtons() {
+  var buttonId = ['#one', '#five', '#ten', '#twenty', '#fifty'];
+  buttonId.map(function(button) {
+    $(button).on('click', function() {
+      thePlayer.placeBet(this.value);
+    })
+  })
+
+  $('#deal').on('click', function() {
+    blackjackDeck.dealCards();
+    $('.inGame').show();
+    $('.preGame').hide();
+  });
+
+  $('#hit').on('click', function() {
+    if (!thePlayer.bust) {
+      blackjackDeck.hit(thePlayer);
+      thePlayer.evaluateCards();
+    } else {
+      alert("You've busted, and cannot hit anymore...")
+    }
+
+  })
+
+  $('#stay').on('click', function() {
+    thePlayer.stay();
+  })
+
+  $('#playAgain').on('click', function() {
+    var playersArray = [thePlayer, theDealer];
+    for (var i=0; i<playersArray.length; i++) {
+      playersArray[i].cardValue = 0;
+      playersArray[i].hand = [];
+      playersArray[i].blackjack = false;
+    }
+    blackjackDeck = new Deck();
+    $('.postGame').hide();
+    $('.preGame').show();
+  })
+
+  $('#leave').on('click', function() {
+
+  })
+
+}
+
+
+
+  var blackjackDeck = new Deck();
+  var thePlayer = new Player("Jon");
+  var theDealer = new Player("Dealer");
+
+
+// Runs all of the necessary functions to set up the game
+$(document).ready(function() {
+  $('.inGame').hide();
+  $('.postGame').hide();
+  bindButtons();
+  blackjackDeck.shuffle();
+
+})
